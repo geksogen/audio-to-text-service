74a7e7a1ee27a2cd5e6b14cd708d232fd069ebfd
Andrey.Torshin <dchorch@gmail.com>
2022-11-01 06:35:34 -0700

    test model max v4

--- backend/app/prepare_models.sh
+++ backend/app/prepare_models.sh
@@ -5,13 +5,11 @@
 #mv vosk-recasepunc-ru-0.22/ recasepunc
 #rm -rf recasepunc.zip
 
-cd ../model_big
 curl -o ./model.zip https://alphacephei.com/vosk/models/vosk-model-ru-0.22.zip
 unzip model.zip
 mv vosk-model-ru-0.22/ model_big
 rm -rf model.zip
 
-cd ../model_smail
 curl -o ./model.zip https://alphacephei.com/vosk/models/vosk-model-small-ru-0.22.zip
 unzip model.zip
 mv vosk-model-small-ru-0.22/ model_smail
